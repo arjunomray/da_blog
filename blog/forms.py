42f9b821170eb4088eb6f4480558d3ba03e905d7
ar-ray0111 <arjunomray@gmail.com>
2023-05-29 14:37:19 +0530

    added a contact form

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=150)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000)
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
-from django.shortcuts import redirect
+from .forms import PostForm, ContactForm
+from django.http import HttpResponse
+
 
 # Create your views here.
 def post_list(request):
 	posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request,"blog/post_list.html", {'posts':posts})
 
+def contact(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
 
+            message = "\n".join(body.values())
+            email = body['email']
+
+            try:
+                send_mail(subject, message, email, ['arjunomray@example.com'])
+            except BadHeaderError:
+                return HttpResponse("Invalid header found")
+            return redirect('post_list')
+    form = ContactForm()
+    return render(request, "blog/contact.html", {"form":form})
 def post_detail(request, pk):
 	
     post = get_object_or_404(Post, pk=pk)
@@ -44,3 +67,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
